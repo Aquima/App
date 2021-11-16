887a5512250260b94aa34096b2298c697a8c3005
Raul Quispe <raul.quispe@live.com>
2021-11-16 09:56:30 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def code():
+    code_value = request.args.get("code", "no code")
+    return "The code is: {}".format(code_value)
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
